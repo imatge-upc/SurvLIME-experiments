f9f36e97eab7f5c844a9e52fd04bbad3419e7aef
CarlosHernandezP <crherperez95@gmail.com>
2022-11-09 11:19:15 +0000

    update experiments

--- survLime/experiments/experiment_xxmm.py
+++ survLime/experiments/experiment_xxmm.py
@@ -20,6 +20,8 @@ from sksurv.ensemble import RandomSurvivalForest
 from survLime import survlime_explainer
 from functools import partial
 
+from derma.general.ingestion.data_loader_csv import SurvivalLoader
+
 from derma.general.preprocessing.transformers import (TransformToNumeric, 
                                                       TransformToDatetime, 
                                                       ComputeAge,
@@ -34,13 +36,11 @@ from derma.general.preprocessing.transformers import (TransformToNumeric,
                                                       CustomScaler,
                                                       CustomImputer)
 
-from derma.general.ingestion.data_loader_csv import SurvivalLoader
-from derma.general.preprocessing.encoders import (CustomOrdinalEncoder as OrdinalEncoder,
+from derma.general.preprocessing.encoders import (OrdinalEncoder,
                                                   GenderEncoder,
                                                   AbsentPresentEncoder,
                                                   LABEncoder,
                                                   CategoricalEncoder)
-from derma.general.ingestion.data_loader_csv import SurvivalLoader
 
 import os
 import config_os as settings_file
@@ -107,65 +107,69 @@ def main(args):
     X['event']    = event
     X.dropna(subset=['duration'],inplace=True)
     X = X[X['duration']>=0]
-    keep_cols = clinical_columns +lab_columns
-    settings_file.keep_cols = {'cols' : keep_cols}
+#   keep_cols = clinical_columns +lab_columns
+#   settings_file.keep_cols = {'cols' : keep_cols}
 
+    X_train_previous, X_test, y_train, y_test = train_test_split(X.copy(), np.zeros(len(X)), test_size=0.1, random_state=42)
     for repetition in tqdm(range(args.repetitions)):
-        if args.model == 'cox':
-            model_pipe = ('coxph', CoxPHSurvivalAnalysis(alpha=0.0001))
-        elif args.model == 'rsf':
-            kwargs = obtain_rsf_kwargs(args)
-            model_pipe = ('rsf', RandomSurvivalForest(
-                              **kwargs,
-                              random_state=42))
-        else:
-            AssertionError('Model not implemented')
+        try:
+            if args.model == 'cox':
+                model_pipe = ('coxph', CoxPHSurvivalAnalysis(alpha=0.0001))
+            elif args.model == 'rsf':
+                kwargs = obtain_rsf_kwargs(args)
+                model_pipe = ('rsf', RandomSurvivalForest(
+                                  **kwargs,
+                                  random_state=42))
+            else:
+                AssertionError('Model not implemented')
 
-        pipe = sklearn.pipeline.Pipeline(steps=[
-            ('TransformToNumeric', TransformToNumeric(**settings_file.transform_to_numeric)), 
-            ('TransformToDatetime', TransformToDatetime(**settings_file.transform_to_datetime)),
-            ('TransformToObject', TransformToObject(**settings_file.transform_to_object)),
-            ('ComputeAge', ComputeAge(**settings_file.compute_age)),
-            ('tr_tm', LinkTumourPartToParent(**settings_file.link_tumour_part_to_parent)),
-            ('tr_cb', TransformCbRegression(**settings_file.transform_cb_regression)),
-            ('tr0', ConvertCategoriesToNaN(**settings_file.convert_categories_to_nan)),
-            ('tr2', GenderEncoder(**settings_file.gender_encoder)),
-            ('tr3', AbsentPresentEncoder(**settings_file.absent_present_encoder)),
-            ("tr4", CategoricalEncoder(**settings_file.categorical_encoder)),
-            ('tr7', LABEncoder(**settings_file.lab_encoder)),
-            ('OrdinalEncoder', OrdinalEncoder(**settings_file.ordinal_encoder)),
-            ('ComputeAJCC', ComputeAJCC(**settings_file.compute_ajcc)),
-            ('tr5', ExponentialTransformer(**settings_file.exponential_transformer)),
-            ('RenameLabValues', RenameLabValues(**settings_file.rename_lab_values)),
+            pipe = sklearn.pipeline.Pipeline(steps=[
+                ('TransformToNumeric', TransformToNumeric(**settings_file.transform_to_numeric)), 
+                ('TransformToDatetime', TransformToDatetime(**settings_file.transform_to_datetime)),
+                ('TransformToObject', TransformToObject(**settings_file.transform_to_object)),
+                ('ComputeAge', ComputeAge(**settings_file.compute_age)),
+                ('tr_tm', LinkTumourPartToParent(**settings_file.link_tumour_part_to_parent)),
+                ('tr_cb', TransformCbRegression(**settings_file.transform_cb_regression)),
+                ('tr0', ConvertCategoriesToNaN(**settings_file.convert_categories_to_nan)),
+                ('tr2', GenderEncoder(**settings_file.gender_encoder)),
+                ('tr3', AbsentPresentEncoder(**settings_file.absent_present_encoder)),
+                ("tr4", CategoricalEncoder(**settings_file.categorical_encoder)),
+                ('tr7', LABEncoder(**settings_file.lab_encoder)),
+                ('OrdinalEncoder', OrdinalEncoder(**settings_file.ordinal_encoder)),
+                ('ComputeAJCC', ComputeAJCC(**settings_file.compute_ajcc)),
+                ('tr5', ExponentialTransformer(**settings_file.exponential_transformer)),
+                ('KeepColumns', KeepColumns(**settings_file.keep_cols)),
+                ('RenameLabValues', RenameLabValues(**settings_file.rename_lab_values)),
+                ('CustomImputer', CustomImputer(strategy='mean')),
+                ('CustomScaler', CustomScaler()),
+                model_pipe
+                ])
+            
 
-            ('KeepColumns', KeepColumns(**settings_file.keep_cols)),
-            ('CustomImputer', CustomImputer(strategy='mean')),
-            ('CustomScaler', CustomScaler()),
-            model_pipe
-            ])
-        
+            
+            X_train, X_val, y_train, y_val = train_test_split(X_train_previous.copy(), np.zeros(len(X_train_previous)), test_size=0.2, random_state=repetition)
+            y_train_list = get_target(X_train)
+            
+            y_train_list = Surv.from_dataframe(*y_train_list.columns, y_train_list)
+            #import ipdb;ipdb.set_trace() 
+            fitted_pipe = pipe.fit(X_train.copy(), y_train_list.copy())
+            X_test_t = fitted_pipe[:-1].transform(X_test)
+            X_train_t = fitted_pipe[:-1].transform(X_train)
 
-        X_train, X_test, y_train, y_test = train_test_split(X.copy(), np.zeros(len(X)), test_size=0.2, random_state=repetition)
-        X_val, X_test, y_val, y_test = train_test_split(X_test.copy(), np.zeros(len(X_test)), test_size=0.5, random_state=repetition)
-        
-        y_train_list = get_target(X_train)
-        
-        y_train_list = Surv.from_dataframe(*y_train_list.columns, y_train_list)
-        
-        fitted_pipe = pipe.fit(X_train.copy(), y_train_list.copy())
-        X_test_t = fitted_pipe[:-1].transform(X_test)
-        X_train_t = fitted_pipe[:-1].transform(X_train)
+            num_neighbors = args.num_neigh
+            explainer = survlime_explainer.SurvLimeExplainer(X_train_t, y_train_list,
+                                                             model_output_times=pipe[-1].event_times_)
 
-        times_to_fill= list(set([x[1] for x in y_train_list]))
-        times_to_fill.sort()
+            computation_exp = compute_weights(explainer, X_test_t, pipe[-1], num_neighbors = num_neighbors)
+            save_path = f"/home/carlos.hernandez/PhD/survlime-paper/survLime/computed_weights_csv/exp4/{args.event_type}/{args.model}_xxmm_surv_{args.event_type}_weights_rand_seed_{repetition}.csv"
+            computation_exp.to_csv(save_path, index=False)
 
-        num_neighbors = args.num_neigh
-        explainer = survlime_explainer.SurvLimeExplainer(X_train_t, y_train_list,
-                                                         model_output_times=pipe[-1].event_times_)
-
-        computation_exp = compute_weights(explainer, X_test_t, pipe[-1], num_neighbors = num_neighbors)
-        save_path = f"/home/carlos.hernandez/PhD/survlime-paper/survLime/computed_weights_csv/exp4/{args.model}_xxmm_surv_weights_rand_seed_{repetition}.csv"
-        computation_exp.to_csv(save_path, index=False)
+        except:
+            print('Error in repetition', repetition)
+            # print traceback
+            import traceback
+            print(traceback.print_exc())
+            pass
 
 def compute_weights(
     explainer: survlime_explainer.SurvLimeExplainer,
@@ -181,11 +185,12 @@ def compute_weights(
             b, _ = explainer.explain_instance(
                 test_point, predict_chf, verbose=False, num_samples=num_pat
             )
-
+                
             b = [x[0] for x in b]
+            compt_weights.append(b)
         except:
             b = [None] * len(test_point)
-        compt_weights.append(b)
+            compt_weights.append(b)
 
     return pd.DataFrame(compt_weights, columns=model.feature_names_in_)
 
@@ -193,7 +198,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Obtain SurvLIME results for a given dataset"
     )
-    parser.add_argument("--event_type", type=str, default="os", help="os, dfs or ds")
+    parser.add_argument("--event_type", type=str, default="ss",
+                        help="os, dfs or ss"
+    )
     parser.add_argument(
         "--model", type=str, default="cox", help="bb model either cox or rsf"
     )
@@ -211,3 +218,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     main(args)
+
