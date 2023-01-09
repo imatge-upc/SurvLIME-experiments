d2da73b18be53cabd8bdfb7ed8b6862f65d61a92
CarlosHernandezP <crherperez95@gmail.com>
2023-01-09 17:41:23 +0000

    exp3 for ML done with repeatable code

--- survLime/experiments/experiment_3.py
+++ survLime/experiments/experiment_3.py
@@ -3,7 +3,7 @@ import warnings
 warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
 import numpy as np
-
+import wandb
 import argparse
 from typing import Union
 from functools import partial
@@ -14,18 +14,92 @@ from tqdm import tqdm
 
 from sksurv.linear_model import CoxPHSurvivalAnalysis
 from sksurv.ensemble import RandomSurvivalForest
+from sksurv.metrics import concordance_index_censored
 from xgbse import XGBSEKaplanNeighbors
 from xgbse.converters import convert_to_structured
 from xgbse.metrics import concordance_index
 from xgbse._kaplan_neighbors import DEFAULT_PARAMS
 
-from survlime.survlime_explainer import SurvLimeExplainer
-from survlime.load_datasets import Loader
+from survlimepy import SurvLimeExplainer
+from survlimepy.load_datasets import Loader
 
 
+def obtain_model(args, model, dataset):
+    if args.model == "cox":
+        model = CoxPHSurvivalAnalysis(alpha=0.0001)
+        type_fn = 'cumulative'
+        predict_chf = partial(model.predict_survival_function, return_array=True)
+    elif args.model == "rsf":
+        if dataset=='udca':
+            args.min_weight_fraction_leaf = 0.045
+            args.min_samples_split = 4
+            args.max_depth = 2
+        else:
+            args.min_weight_fraction_leaf = 0
+            args.min_samples_split = 2
+            args.max_depth = 3
+        model = RandomSurvivalForest(n_estimators=args.n_estimators,
+                                        max_features=args.max_features,
+                                        min_samples_split=args.min_samples_split,
+                                        min_weight_fraction_leaf=args.min_weight_fraction_leaf,
+                                        max_depth=args.max_depth,
+                                        n_jobs=-1, random_state=42)
+        type_fn = 'cumulative'
+        predict_chf = partial(model.predict_survival_function, return_array=True)
+    elif args.model == 'xgb':
+        if dataset=='lung':
+            alpha = 0.001; lr = 0.00507978667521773
+            max_depth = 28; min_child_weight = 8
+        else:
+            alpha = args.alpha; lr =args.lr
+            max_depth = args.max_depth; min_child_weight = args.min_child_weight
+        DEFAULT_PARAMS = {
+            'objective': 'survival:aft',
+            'eval_metric': 'aft-nloglik',
+            'aft_loss_distribution': args.aft_loss_distribution,
+            'aft_loss_distribution_scale': args.aft_loss_distribution_scale,
+            'tree_method': 'hist', 
+            'learning_rate': lr, 
+            'max_depth': max_depth,
+            'alpha': alpha,
+            'min_child_weight': min_child_weight,
+        }
+        model = XGBSEKaplanNeighbors(DEFAULT_PARAMS, n_neighbors=50)
+        predict_chf = model.predict
+        type_fn = 'survival'
+    else:
+        raise AssertionError
+
+    return model, predict_chf, type_fn
+
+
+
+def obtain_c_index(args, dataset, model, test, train, x):
+    # plot concordance index on test set
+    preds = model.predict(test[0])
+    preds_train = model.predict(train[0])
+    if args.model == 'xgb':
+        c_index = concordance_index(test[1], preds)
+        c_index_train = concordance_index(train[1], preds_train)
+    else:
+        event_indicator = [x[0] for x in test[1]]
+        event_time = [x[1] for x in test[1]]
+        c_index = concordance_index_censored(event_indicator, event_time, preds)[0]
+        # compute c_index_train
+        event_indicator = [x[0] for x in train[1]]
+        event_time = [x[1] for x in train[1]]
+        c_index_train = concordance_index_censored(event_indicator, event_time, preds_train)[0]
+
+    print(f'Dataset: {dataset} and model  {args.model}')
+    print(f"Concordance index on test set: {c_index:.3f}")
+    #wandb.log({"test c-index": c_index, "train c-index": c_index_train})
+
+
+np.random.seed(42)
 def main(args):
+    #wandb.init(project="survlime", config=args)
     if args.dataset == "all":
-        datasets = ["veterans", "udca", "lung", "pbc"]
+        datasets = [ "udca", "lung", "pbc", "veterans"]
     else:
         datasets = [args.dataset]
     if args.model != "all":
@@ -33,57 +107,37 @@ def main(args):
     else:
         models = ["cox", "rsf",'xgb']
     for model in models:
+        print("-"*50)
         args.model = model
         for dataset in datasets:
+            loader = Loader(dataset_name=dataset)
+            x, events, times = loader.load_data()
+
+            train, _, test = loader.preprocess_datasets(x, events, times, random_seed=0)
+            model, predict_chf, type_fn = obtain_model(args, model, dataset)
+
+            model.fit(train[0], train[1])
+            obtain_c_index(args, dataset, model, test, train, x)
+            continue
+
+            if args.model == "cox":
+                model_output_times = model.event_times_
+            elif args.model == "rsf":
+                model_output_times = model.event_times_
+            elif args.model == 'xgb':
+                model_output_times = model.time_bins
+            else:
+                raise AssertionError
+            events_train = [1 if x[0] else 0 for x in train[1]]
+            times_train = [x[1] for x in train[1]]
+            explainer = SurvLimeExplainer(
+                    training_features=train[0],
+                    training_events=[True if x==1 else False for x in events_train],
+                    training_times=times_train,
+                    model_output_times=model_output_times,
+                    random_state=10,
+            )
             for i in tqdm(range(args.repetitions)):
-                loader = Loader(dataset_name=dataset)
-                x, events, times = loader.load_data()
-
-                train, _, test = loader.preprocess_datasets(x, events, times, random_seed=i)
-
-                if args.model == "cox":
-                    model = CoxPHSurvivalAnalysis(alpha=0.0001)
-                    model_output_times = model.event_times_
-                    type_fn = 'cumulative'
-                elif args.model == "rsf":
-                    model = RandomSurvivalForest()
-                    model_output_times = model.event_times_
-                    type_fn = 'cumulative'
-                elif args.model == 'xgb':
-                    DEFAULT_PARAMS =  {'objective': 'survival:aft',
-                                                  'eval_metric': 'aft-nloglik',
-                                                  'aft_loss_distribution': 'normal',
-                                                  'aft_loss_distribution_scale': 1.20,
-                                                  'tree_method': 'hist', 'learning_rate': 0.05, 'max_depth': 10}
-                    model = XGBSEKaplanNeighbors(DEFAULT_PARAMS, n_neighbors=50)
-                    predict_chf = model.predict
-                    type_fn = 'survival'
-                else:
-                    raise AssertionError
-                model.fit(train[0], train[1])
-                # plot concordance index on test set
-                preds = model.predict(train[0])
-                c_index = concordance_index(train[1], preds)
-                print(f"Concordance index on test set: {c_index:.3f}")
-                if args.model == "cox":
-                    model_output_times = model.event_times_
-                elif args.model == "rsf":
-                    model_output_times = model.event_times_
-                elif args.model == 'xgb':
-                    model_output_times = model.time_bins
-                else:
-                    raise AssertionError
-
-                events_train = [1 if x[0] else 0 for x in train[1]]
-                times_train = [x[1] for x in train[1]]
-                explainer = SurvLimeExplainer(
-                        training_features=train[0],
-                        training_events=[True if x==1 else False for x in events_train],
-                        training_times=times_train,
-                        model_output_times=model_output_times,
-                        sample_around_instance=True,
-                        random_state=10,
-                )
 
                 computation_exp = compute_weights(explainer, 
                                                   test[0].astype('float32'),
@@ -106,22 +160,14 @@ def compute_weights(
     compt_weights = []
     num_pat = num_neighbors
     for i, test_point in enumerate(tqdm(x_test.to_numpy())):
-        try:
-            b = explainer.explain_instance(
-                test_point, predict_chf,
-                verbose=False, num_samples=num_pat,
-                type_fn = type_fn
-            )
+        b = explainer.explain_instance(
+            test_point, predict_chf,
+            verbose=False, num_samples=num_pat,
+            type_fn = type_fn,
+        )
 
-        except:
-            import traceback
-            traceback.print_exc()
-            print(f"Error in the computation of the weights in patient {i}")
-            b = [None] * len(test_point)
         compt_weights.append(b)
-
     return pd.DataFrame(compt_weights, columns=column_names)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -149,5 +195,47 @@ if __name__ == "__main__":
         default=1000,
         help="Number of neighbours to use for the explanation",
     )
+
+
+
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+    parser.add_argument('--reg', type=float, default=0.001, help='Regularization')
+
+    # XGboost
+    parser.add_argument('--aft_loss_distribution', type=str, default='normal')
+    parser.add_argument('--aft_loss_distribution_scale', type=float, default=1.0)
+    parser.add_argument('--min_child_weight', type=float, default=1)
+    parser.add_argument('--gamma', type=float, default=0.001) # <- monomum loss reduction required to make further partition
+    
+    # Machine learning Arguments
+    parser.add_argument('--n_iter', type=int, default=101)
+    parser.add_argument('--alpha', type=float, default=0.0001)
+    parser.add_argument('--tol', type=float, default=1e-9)
+    parser.add_argument('--n_estimators', type=int, default=100)
+    parser.add_argument('--max_depth', type=int, default=3)
+    parser.add_argument('--min_samples_split', type=int, default=2)
+    parser.add_argument('--min_weight_fraction_leaf', type=float, default=0.0)
+    parser.add_argument('--max_features', type=str, default='auto')
     args = parser.parse_args()
+
     main(args)
+
+"""
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+    parser.add_argument('--reg', type=float, default=0.001, help='Regularization')
+
+    # XGboost
+    parser.add_argument('--aft_loss_distribution', type=str, default='normal')
+    parser.add_argument('--aft_loss_distribution_scale', type=float, default=1.0)
+    parser.add_argument('--min_child_weight', type=float, default=1)
+    parser.add_argument('--gamma', type=float, default=0.001) # <- monomum loss reduction required to make further partition
+    
+    # Machine learning Arguments
+    parser.add_argument('--n_iter', type=int, default=101)
+    parser.add_argument('--alpha', type=float, default=0.0001)
+    parser.add_argument('--tol', type=float, default=1e-9)
+    parser.add_argument('--n_estimators', type=int, default=100)
+    parser.add_argument('--max_depth', type=int, default=3)
+    parser.add_argument('--min_samples_split', type=int, default=2)
+    parser.add_argument('--min_weight_fraction_leaf', type=float, default=0.0)
+"""
--- survLime/experiments/experiment_3_dl.py
+++ survLime/experiments/experiment_3_dl.py
@@ -1,3 +1,4 @@
+import wandb
 import warnings
 warnings.filterwarnings("ignore")
 import matplotlib.pyplot as plt
@@ -11,9 +12,6 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from sksurv.linear_model import CoxPHSurvivalAnalysis
-from sksurv.ensemble import RandomSurvivalForest
-
 # DL for survival
 import pycox
 from pycox.models import CoxPH, DeepHitSingle
@@ -21,29 +19,36 @@ from pycox.models import CoxPH, DeepHitSingle
 from sklearn_pandas import DataFrameMapper
 import torchtuples as tt
 from pycox.evaluation import EvalSurv
+import torch
 
+from survlimepy import SurvLimeExplainer
+from survlimepy.load_datasets import Loader
 
-from survlime.survlime_explainer import SurvLimeExplainer
-from survlime.load_datasets import Loader
+# add seeds for reproducibility
+torch.manual_seed(42)
+np.random.seed(42)
 
-
-def train_model(args, data, label):
+def train_model(args, data, label, dataset):
     """ Documentation train loop
     Arguments:
     args: Arguments from the parser
     data: Data to train
     label: Label to train
     """
-   #y_train = get_target(label[0])
-   #y_val   = get_target(label[1])
-
+    if args.model == 'deephit':
+        if dataset == 'pbc':
+            lr = 0.001
+        else:
+            lr = args.lr
+    print(lr)
     # Transform data into numpy arrays
   # leave = [(col, None) for col in data[0].columns]
   # x_mapper = DataFrameMapper(leave)
   # x_train = x_mapper.fit_transform(data[0]).astype('float32')
   # x_val = x_mapper.transform(data[1]).astype('float32')
-    label[0] = (np.asarray(label[0][0]), np.asarray(label[0][1]))
-    label[2] = (np.asarray(label[2][0]), np.asarray(label[2][1]))
+    label[0] = (np.asarray(label[0][1]), np.asarray(label[0][0]))
+    label[1] = (np.asarray(label[1][1]), np.asarray(label[1][0]))
+    label[2] = (np.asarray(label[2][1]), np.asarray(label[2][0]))
 
     # Model Hyperparms
     num_nodes = [args.num_nodes for i in range(args.num_layers)]
@@ -59,7 +64,8 @@ def train_model(args, data, label):
     elif args.model =='deephit':
         num_durations = 20
         labtrans = DeepHitSingle.label_transform(num_durations)
-        y_train = labtrans.fit_transform(label[0][0], label[0][1])
+        y_train = labtrans.fit_transform(*label[0])
+        label[1] = labtrans.transform(*label[1])
         out_features = labtrans.out_features
     
     # Instantiate the model
@@ -67,11 +73,13 @@ def train_model(args, data, label):
                               dropout, output_bias=output_bias)
     
     if args.model == 'deepsurv':
-        model = pycox.models.CoxPH(net, tt.optim.Adam(weight_decay=args.reg))
+        model = CoxPH(net, tt.optim.Adam(weight_decay=args.reg))
     elif args.model == 'deephit':
         model = DeepHitSingle(net, tt.optim.Adam(weight_decay=args.reg), duration_index=labtrans.cuts)
-    callbacks = [tt.callbacks.EarlyStopping(patience=10)]
-    model.optimizer.set_lr(args.lr)
+    callbacks = [tt.callbacks.EarlyStopping(patience=5)]
+
+    
+    model.optimizer.set_lr(lr)
 
     x_train = data[0].astype('float32').to_numpy()
     x_val = data[1].astype('float32').to_numpy()
@@ -82,8 +90,8 @@ def train_model(args, data, label):
     # Train!
     log = model.fit(input=x_train, target=y_train, batch_size=batch_size,
                     epochs=epochs, callbacks=callbacks, verbose=False,
-                    val_data=(x_val, y_train)
-                        )
+                    val_data=(x_val, label[1]))
+                        
     # if the output of the model is of length one
     if out_features == 1:
         model.compute_baseline_hazards()
@@ -96,19 +104,28 @@ def train_model(args, data, label):
 
         # Compte the C-index on the test set
         y_test = label[2]
-        surv = model.predict_surv_df(x_test)
-        ev = EvalSurv(surv, y_test[0], y_test[1], censor_surv='km')
-        c_index_test = ev.concordance_td('antolini')
+        surv_test = model.predict_surv_df(x_test)
+        #surv = surv.T
+        ev_test = EvalSurv(surv_test, y_test[0], y_test[1], censor_surv='km')
+        c_index_test = ev_test.concordance_td('antolini')
     except:
-        c_index = 0
+        # print traceback
+        import traceback
+        traceback.print_exc()
+        #c_index = 0
         c_index_test = 0
+        print('Peto')
+    #wandb.log({'c_index': c_index, 'test c-index': c_index_test})
+    print(f'Model: {args.model} -:- Dataset: {dataset}')
     print(f"C-index train: {c_index:.3f}")
     print(f"C-index test: {c_index_test:.3f}")
 
     return model
+
 def main(args):
+    #wandb.init(project="survlime", config=args)
     if args.dataset == "all":
-        datasets = ["veterans", "udca", "lung", "pbc"]
+        datasets = ["udca", "lung", "pbc", "veterans"]
     else:
         datasets = [args.dataset]
     if args.model != "all":
@@ -116,58 +133,60 @@ def main(args):
     else:
         models = ["deepsurv", "deephit"]
     for model in models:
+        print("-"*50)
         args.model = model
         for dataset in datasets:
+            loader = Loader(dataset_name=dataset)
+            x, events, times = loader.load_data()
+
+            train, val, test = loader.preprocess_datasets(x, events, times, random_seed=0)
             for i in tqdm(range(args.repetitions)):
-                loader = Loader(dataset_name=dataset)
-                x, events, times = loader.load_data()
-
-                train, _, test = loader.preprocess_datasets(x, events, times, random_seed=i)
-
                 # Prepare datasets
                 events_train = [1 if x[0] else 0 for x in train[1]]
                 times_train = [x[1] for x in train[1]]
                 train[1] = (events_train, times_train)
+                
+                events_val = [1 if x[0] else 0 for x in val[1]]
+                times_val = [x[1] for x in val[1]]
+                val[1] = (events_val, times_val)
 
                 events_test = [1 if x[0] else 0 for x in test[1]]
                 times_test = [x[1] for x in test[1]]
                 test[1] = (events_test, times_test)
 
-                data = [train[0], train[0], test[0]]
-                label = [train[1], train[1], test[1]]
+                data = [train[0], val[0], test[0]]
+                label = [train[1], val[1], test[1]]
+                model = train_model(args, data, label, dataset)
 
-                model = train_model(args, data, label)
-
-
+                continue
                 times_to_fill = list(set([x[1] for x in train[1]]))
                 times_to_fill.sort()
                 #H0 = model.cum_baseline_hazard_.y.reshape(len(times_to_fill), 1)
-
+                def pred_funct(model,x):
+                    return model.predict_surv_df(x).T
                 if args.model == 'deepsurv':
-                    predict_chf = model.predict_cumulative_hazards
+                    predict_chf = model.predict_surv_df
+                    predict_chf= partial(pred_funct, model)
                    #chosen_H0 = model.baseline_hazards_.to_numpy()
                    #chosen_H0 = np.reshape(chosen_H0, (chosen_H0.shape[0],1))
                     #print(chosen_H0.shape)
                     model_output_times = times_to_fill
-                    type_fn = 'cumulative'
-                else:
-                    #import ipdb; ipdb.set_trace()
                     type_fn = 'survival'
-                    predict_chf = model.predict_surv
+                else:
+                    type_fn = 'survival'
+                    predict_chf = model.predict_surv_df
                     model_output_times = model.duration_index
 
                 explainer = SurvLimeExplainer(
                         training_features=train[0].astype('float32').to_numpy(),
-                        traininig_events=[True if x==1 else False for x in events_train],
+                        training_events=[True if x==1 else False for x in events_train],
                         training_times=times_train,
                         model_output_times=model_output_times,
-                        sample_around_instance=True,
                         random_state=10,
                 )
 
                 computation_exp = compute_weights(explainer, 
                                                   test[0].astype('float32'),
-                                                  model = model,
                                                   predict_chf = predict_chf,
                                                   column_names = test[0].columns,
                                                   num_neighbors = args.num_neighbours,
@@ -183,7 +202,6 @@ def compute_weights(
     x_test: np.ndarray,
     column_names: List[str],
     predict_chf: Callable,
-    model: Union[CoxPHSurvivalAnalysis, RandomSurvivalForest],
     num_neighbors: int = 1000,
     type_fn: str = 'cumulative'
 ):
@@ -191,19 +209,17 @@ def compute_weights(
     num_pat = num_neighbors
     #predict_chf = partial(model.predict_cumulative_hazard_function, return_array=True)
     for i, test_point in enumerate(tqdm(x_test.to_numpy())):
-        try:
-            b = explainer.explain_instance(
-                test_point, predict_chf,
-                verbose=False, num_samples=num_pat,
-                type_fn = type_fn
-            )
+        b = explainer.explain_instance(
+            test_point, predict_chf,
+            verbose=False, num_samples=num_pat,
+            type_fn = type_fn
+        )
 
-        except:
-            #print traceback
-            import traceback
-            traceback.print_exc()
-            print(f"Error in the computation of the weights in patient {i}")
-            b = [None] * len(test_point)
+#           #print traceback
+#           import traceback
+#           traceback.print_exc()
+#           print(f"Error in the computation of the weights in patient {i}")
+#           b = [None] * len(test_point)
         compt_weights.append(b)
 
     return pd.DataFrame(compt_weights, columns=column_names)
@@ -220,9 +236,8 @@ if __name__ == "__main__":
         help="either veterans, lungs, udca or pbc, or all",
     )
     parser.add_argument(
-        "--model", type=str, default="cox", help="bb model either cox or rsf"
+        "--model", type=str, default="deepsurv", help="either deepsurv or deephit"
     )
-    parser.add_argument("--rs", type=int, default=0, help="Random seed for the splits")
     parser.add_argument(
         "--repetitions",
         type=int,
@@ -237,13 +252,52 @@ if __name__ == "__main__":
     )
 
     parser.add_argument('--num_neighbours', type=int, default=1000, help='Number of neighbours to use')
+
     # add repetitions
-    parser.add_argument('--num_layers', type=int, default=1)
-    parser.add_argument('--num_nodes', type=int, default=16)
     parser.add_argument('--batch_norm', type=bool, default=False)
-    parser.add_argument('--dropout', type=float, default=0.01)
     parser.add_argument('--output_bias', type=bool, default=False)
-    parser.add_argument('--reg', type=float, default=0.001)
-    parser.add_argument('--lr', type=float, default=0.1)
+    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--num_nodes', type=int, default=32)
+    parser.add_argument('--dropout', type=float, default=0.2)
+    parser.add_argument('--reg', type=float, default=0.0001 )
+    parser.add_argument('--lr', type=float, default=0.001)
+
     args = parser.parse_args()
+    print(args)
     main(args)
+# These work sufficiently good for DS [udca, lung, veterans] and DH [udca, pbc]
+# PBC deephit
+   #parser.add_argument('--num_layers', type=int, default=2)
+   #parser.add_argument('--num_nodes', type=int, default=32)
+   #parser.add_argument('--batch_norm', type=bool, default=False)
+   #parser.add_argument('--dropout', type=float, default=0.2)
+   #parser.add_argument('--output_bias', type=bool, default=False)
+   #parser.add_argument('--reg', type=float, default=0.0001 )
+   #parser.add_argument('--lr', type=float, default=0.001)
+
+
+   #parser.add_argument('--num_layers', type=int, default=1)
+   #parser.add_argument('--num_nodes', type=int, default=32)
+   #parser.add_argument('--batch_norm', type=bool, default=False)
+   #parser.add_argument('--dropout', type=float, default=0.3)
+   #parser.add_argument('--output_bias', type=bool, default=False)
+   #parser.add_argument('--reg', type=float, default=0.0001)
+   #parser.add_argument('--lr', type=float, default=0.2)
+
+# For DeepSurv and PBC
+
+   #parser.add_argument('--num_layers', type=int, default=2)
+   #parser.add_argument('--num_nodes', type=int, default=8)
+   #parser.add_argument('--batch_norm', type=bool, default=False)
+   #parser.add_argument('--dropout', type=float, default=0.2)
+   #parser.add_argument('--output_bias', type=bool, default=False)
+   #parser.add_argument('--reg', type=float, default=0.0001)
+   #parser.add_argument('--lr', type=float, default=0.01)
+
+   #parser.add_argument('--num_layers', type=int, default=2)
+   #parser.add_argument('--num_nodes', type=int, default=16)
+   #parser.add_argument('--batch_norm', type=bool, default=False)
+   #parser.add_argument('--dropout', type=float, default=0.2)
+   #parser.add_argument('--output_bias', type=bool, default=False)
+   #parser.add_argument('--reg', type=float, default=0.0001)
+   #parser.add_argument('--lr', type=float, default=0.01)
