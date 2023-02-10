8d88aad5a93fc51939e8ddb3eeb7efb6af31d42d
pachoning <cc.pachon@gmail.com>
2023-02-10 13:00:17 +0100

    Place weights forder in the same directory as script

--- survLime/experiments/make_plots_script.py
+++ survLime/experiments/make_plots_script.py
@@ -12,6 +12,14 @@ from matplotlib.ticker import StrMethodFormatter
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics import mean_squared_error
 
+def change_name_simulated(name):
+    if name=="min":
+        return "best"
+    elif name=="max":
+        return "worst"
+    else:
+        return name
+
 def change_dataset(name):
     if name=="veterans":
         return "Veteran"
@@ -79,15 +87,16 @@ def create_boxenplot(data):
     return p
 
 
-
 def generate_plots_real_datasets():
-    results_folder = os.path.join(os.path.dirname(os.getcwd()), "computed_weights_csv", "exp_real_datasets")
+    results_folder = os.path.join(os.getcwd(), "computed_weights_csv", "exp_real_datasets")
 
     models = ["cox", "rsf","xgb"]#"deepsurv","deephit"]
     datasets = ["lung", "veterans", "udca"] # "pbc"
     for dataset in datasets:
         for model_type in models:
-            data = pd.read_csv(results_folder+f"/{model_type}_exp_{dataset}_surv_weights.csv")
+            file_name = f"{model_type}_exp_{dataset}_surv_weights.csv"
+            file_path = os.path.join(results_folder, file_name)
+            data = pd.read_csv(file_path)
             data = data.reindex(data.median().sort_values(ascending=False).index, axis=1)
             # if there is a columned named ph.ecog_2.0 rename it to ph.ecog_2
             if "ph.ecog_2.0" in data.columns:
@@ -98,23 +107,17 @@ def generate_plots_real_datasets():
             dataset_name = change_dataset(dataset)
             p.set_title(f"{model_name} {dataset_name} SurvLIME values", fontsize= 16, fontweight="bold");
 
-            plt.savefig(f"figures/{model_type}_{dataset}.png",  bbox_inches="tight", dpi=200)
-            print(f"Figure saved in figures/{model_type}_{dataset}.png")
+            fig_name = f"{model_type}_{dataset}.png"
+            fig_path = os.path.join("figures", fig_name)
+            plt.savefig(fig_path,  bbox_inches="tight", dpi=200)
+            print(f"Figure saved in {fig_path}")
 
-
-def change_name_simulated(name):
-    if name=="min":
-        return "best"
-    elif name=="max":
-        return "worst"
-    else:
-        return name
 
 def generate_plots_simulated_experiments():
     for i in [1, 2]:
         for group in ["mean","min","max"]:
             name_file = f"exp_2_cluster_{i}_{group}.csv"
-            data_folder = os.path.join(os.path.dirname(os.getcwd()), "computed_weights_csv", "exp2")
+            data_folder = os.path.join(os.getcwd(), "computed_weights_csv", "exp2")
             file_path =  os.path.join(data_folder, name_file)
 
             data = pd.read_csv(file_path)
@@ -126,8 +129,10 @@ def generate_plots_simulated_experiments():
 
             p.set_title(title, fontsize= 18, fontweight="bold");
 
-            plt.savefig(f"figures/simulated_exp_cluster{i}_{group}_values.png",  bbox_inches="tight", dpi=200)
-            print(f"Figure saved in figures/simulated_exp_cluster{i}_{group}_values.png")
+            fig_name = f"simulated_exp_cluster{i}_{group}_values.png"
+            fig_path = os.pardir("figures", fig_name)
+            plt.savefig(fig_path,  bbox_inches="tight", dpi=200)
+            print(f"Figure saved in {fig_path}")
     
 
 def generate_deepmodels_rds_plots():
@@ -135,19 +140,43 @@ def generate_deepmodels_rds_plots():
     This function generates the plots for the deepsurv_rds experiment
     and saves the result in the figures folder
     """
-    results_folder = os.path.join(os.path.dirname(os.getcwd()), "computed_weights_csv", "exp_deepsurv_rds")
-    data = pd.read_csv(results_folder+f"/exp_deepsurv_rds_surv_weights.csv")
+    results_folder = os.path.join(os.getcwd(), "computed_weights_csv", "exp_deepsurv_rds")
+    file_path = os.path.join(results_folder, "exp_deepsurv_rds_surv_weights.csv")
+    data = pd.read_csv(file_path)
     
     p = create_boxenplot(data)
 
     p.set_title(f"DeepSurv RSD SurvLIME values", fontsize= 16, fontweight="bold");
 
-    plt.savefig(f"figures/deepsurv_rds.png",  bbox_inches="tight", dpi=200)
-    print("Figure saved in figures/deepsurv_rds.png")
+    fig_name = "deepsurv_rds.png"
+    fig_path = os.path.join("figures", fig_name)
+    plt.savefig(fig_path,  bbox_inches="tight", dpi=200)
+    print(f"Figure saved in {fig_path}")
+
+def generate_plot_single_point(cluster):
+    data_folder = os.path.join(os.getcwd(), "computed_weights_csv", "exp2")
+    if cluster == 1:
+        file_name = "center_cluster_1.csv"
+        title = "Cluster 1: Values of the coefficients"
+        fig_name = "center_cluster_1.png"
+    else:
+        file_name = "center_cluster_2.csv"
+        title = "Cluster 2: Values of the coefficients"
+        fig_name = "center_cluster_2.png"
+    file_path = os.path.join(data_folder, file_name)
+    data = pd.read_csv(file_path, index_col=0)
+    p = data.transpose().plot.bar(
+        color={"CoxPH": "blue", "SurvLIME": [1.0, 0.55, 0], "Real": "green"},
+        rot = 45,
+        title = title,
+        fontsize = 14
+    )
+    fig_path = os.path.join("figures", fig_name)
+    p.figure.savefig(fig_path)
 
 
 def plot_experiment_1():
-    w_path = "results/"
+    w_path = "results"
     # Falta escribir el código para la Figure 3 del paper
     coefficients_1 = [10**(-6), 0.1,  -0.15, 10**(-6), 10**(-6)]
     coefficients_2 = [10**(-6), -0.15, 10**(-6), 10**(-6), -0.1]
@@ -156,7 +185,9 @@ def plot_experiment_1():
     min_dfs  = []
     for exp, coefficients in zip([1,2], [coefficients_1, coefficients_2]):
         for i in tqdm(range(50)):
-            compt_weights = pd.read_csv(w_path+f"cox_exp_1.{exp}_rand_seed_{i}_surv_weights_na.csv")
+            file_name = f"cox_exp_1.{exp}_rand_seed_{i}_surv_weights_na.csv"
+            file_path = os.path.join(w_path, file_name)
+            compt_weights = pd.read_csv(file_path)
             compt_weights = compt_weights.dropna(axis=0)
             compt_weights.columns = ["One", "Two", "Three", "Four","Five"]
             
@@ -204,7 +235,10 @@ def plot_experiment_1():
         plt.gca().yaxis.set_major_formatter(StrMethodFormatter("{x:,.2f}"))
         
         # Save the figure
-        plt.savefig(f"figures/exp_1.{exp}_results.png", bbox_inches = "tight", dpi=200)   
+        fig_name = f"exp_1.{exp}_results.png"
+        fig_path = os.path.join("figures", fig_name)
+        plt.savefig(fig_path, bbox_inches = "tight", dpi=200)
+        print(f"Figure saved in {fig_path}")
 
 if __name__ == "__main__":
     # if directory figures does not exist create it
