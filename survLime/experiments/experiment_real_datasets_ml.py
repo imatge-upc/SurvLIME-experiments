a3da6e7eae18329f62989329efa3f4c2a952e819
pachoning <cc.pachon@gmail.com>
2023-02-10 12:58:55 +0100

    Place weights forder in the same directory as script

--- survLime/experiments/experiment_real_datasets_ml.py
+++ survLime/experiments/experiment_real_datasets_ml.py
@@ -170,9 +170,7 @@ def exp_real_datasets(args_org):
 
     args = copy.deepcopy(args_org)
 
-    save_dir = os.path.join(
-        os.path.dirname(os.getcwd()), "computed_weights_csv", "exp_real_datasets"
-    )
+    save_dir = os.path.join(os.getcwd(), "computed_weights_csv", "exp_real_datasets")
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     datasets, models = models_and_datasets(args)
